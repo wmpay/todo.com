a6fd3f17a5d02e29b9ce3f37999fbd09143afc59
Max Paymar <William_Paymar@trepp.com>
2018-03-26 17:25:14 -0400

    added modal dependancy

--- web/src/components/toolbar/index.js
+++ web/src/components/toolbar/index.js
@@ -1,17 +1,18 @@
 import Vue from 'vue';
 import template from './toolbar.html';
 import todoService from '../../services/todo';
+import todoEditorModalTemplate from './todoEditorModal.html';
 
 export default {
   data: () => {
     return {
-      showTodoEditor: false,
     };
   },
   methods: {
     createNewTodo: function () {
-      console.log('button pressed');
-      console.log(this.$data.showTodoEditor);
+      this.$modal.show({
+        template: todoEditorModalTemplate,
+      });
     },
   },
   template: template,
