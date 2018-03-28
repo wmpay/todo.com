5903694759f9551a73bdc0056bf216a08ceecf4c
Max Paymar <wmpaymar@gmail.com>
2018-03-27 21:25:57 -0400

    implement filter toggling

--- web/src/components/toolbar/index.js
+++ web/src/components/toolbar/index.js
@@ -2,6 +2,14 @@ import template from './toolbar.html';
 import todoEditor from '../todoEditor/index';
 
 export default {
+  data: () => {
+    return {
+      filterState: {
+        dueSoon: false,
+        pastDue: false,
+      },
+    };
+  },
   methods: {
     createNewTodo: function () {
       this.$modal.show(todoEditor, {
@@ -10,6 +18,15 @@ export default {
         minHeight: 450,
         name: 'todoEditor',
       });
+    },
+    toggleFilterState: function(filter) {
+      Object.keys(this.filterState).forEach((key) => {
+        if (key !== filter) {
+          this.filterState[key] = false;
+        }
+      });
+      this.filterState[filter] = !this.filterState[filter];
+      this.$parent.reloadTodos(this.filterState);
     },
   },
   template: template,
